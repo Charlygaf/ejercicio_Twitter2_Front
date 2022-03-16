51aebeb7775198e4fbbb352854157e15c57bc89a
Charlygaf <charlygaf@gmail.com>
2022-03-16 15:20:19 -0300

    home

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -6,48 +6,48 @@ function RighSidebar() {
   const users = useSelector((state) => state);
 
   return (
-    <section class="">
+    <section className="">
       {/* <!-- sticky Search Nav --> */}
-      <div class="row bg-black sticky-top py-2">
-        <form class="d-flex justify-content-between">
+      <div className="row bg-black sticky-top py-2">
+        <form className="d-flex justify-content-between">
           <input
-            class="form-control"
+            className="form-control"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button class="btn btn-outline-primary" type="submit">
+          <button className="btn btn-outline-primary" type="submit">
             Search
           </button>
         </form>
       </div>
       {/* <!-- Friend suggestions --> */}
-      <div class="friend-suggestions bg-dark p-3 mt-2 rounded w-100">
-        <h4 class="text-light">You might like</h4>
+      <div className="friend-suggestions bg-dark p-3 mt-2 rounded w-100">
+        <h4 className="text-light">You might like</h4>
 
-        <div class="row">
-          <div class="col m-0 p-0">
-            <ul class="sidebar mx-1 my-0 p-0">
+        <div className="row">
+          <div className="col m-0 p-0">
+            <ul className="sidebar mx-1 my-0 p-0">
               {users.slice(0, 5).map((user) => (
-                <li class="">
+                <li className="">
                   <form
-                    class="d-flex justify-content-between align-items-center me-0"
+                    className="d-flex justify-content-between align-items-center me-0"
                     action="/follow"
                     method="POST"
                   >
                     <Link to={`/${user.firstname}`}>
                       <img
-                        className="rounded-circle w-15 side-bar-icon d-inline m-0"
+                        classNameName="rounded-circle w-15 side-bar-icon d-inline m-0"
                         src={`/img/${user.photoProfile}`}
                         alt="Profile_Img"
                       />
-                      <p class="d-none d-lg-inline"> {user.firstname}</p>
+                      <p className="d-none d-lg-inline"> {user.firstname}</p>
                     </Link>
                     <input type="hidden" name="objectId" value={user.id} />
 
                     <button
                       type="submit"
-                      class="btn btn-primary rounded-pill font-register"
+                      className="btn btn-primary rounded-pill font-register"
                     >
                       follow
                     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,37 @@ import React from "react";
 import { useSelector } from "react-redux";
 import LeftSidebar from "../components/LeftSidebar";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import RighSidebar from "../components/RightSidebar";
+import axios from "axios";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const users = await axios.get("http://localhost:8000/users");
+        setUsers(users);
+        console.log(users);
+      } catch (error) {
+        console.log("error");
+      }
+    }
+    getUsers();
+  }, []);
+
   return (
-    <div className="container">
+    <div className="container row">
       <Navbar />
       <div className="col-lg-3">
         <LeftSidebar />
       </div>
-      <div className="col-lg-3"></div>
-      <div className="col-lg-3"></div>
+      <div className="col-lg-6"></div>
+
+      <div className="col-lg-3">
+        <RighSidebar users={users} />
+      </div>
     </div>
   );
 }
