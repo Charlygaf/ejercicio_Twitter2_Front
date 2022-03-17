3fba61d825c7e392a8dbc17c4e683f85ea255d62
Valentina Giusti <valentinamgiusti@gmail.com>
2022-03-17 13:00:35 -0300

    x

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -3,7 +3,17 @@ import axios from "axios";
 function userReducer(state = [], action) {
   switch (action.type) {
     case "ADD_USER":
-      return state;
+      return [
+        ...state,
+        {
+          firstname: action.payload,
+          lastname: action.payload,
+          userName: action.payload,
+          email: action.payload,
+          birthDate: action.payload,
+          password: action.payload,
+        },
+      ];
     case "ADD_FOLLOWER":
       return state;
     case "REMOVE_FOLLOWER":
